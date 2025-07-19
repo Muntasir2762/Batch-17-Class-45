d8d974351b917b01819f0b03ff5f712e2c4f4fcc
Munotasir2762 <muntasirpranto2762@gmail.com>
2025-07-19 22:36:14 +0600

    First class on PHP

---
+++ index.php
@@ -0,0 +1,29 @@
+    <?php
+
+    echo "<h1>This is our first php code!</h1>";
+
+    $number1 = 20;
+    $number2 = 30;
+
+    $result = $number1+$number2;
+
+    echo "The result is=".$result."<br>";
+
+    for($i=1; $i<=5; $i++){
+        echo "Hello world!! <br>";
+    }
+
+    ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP First Class</title>
+</head>
+<body>
+    <h1>This is our first class on PHP</h1>
+</body>
+</html>
+
